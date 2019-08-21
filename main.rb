9641dfe8a5120f270ac1e5e661a061b66872b9e8
Jon Stokes <jon.m.stokes@gmail.com>
2019-08-21 11:30:27 -0500

    Started parsing SMS file

--- main.rb
+++ main.rb
@@ -1,11 +1,29 @@
 require 'csv'
 require 'pry'
 
+# Common functions
+def read_sms_irradiance_log(filename)
+  begin
+    File.read(filename).split("\n").map do |line|
+      line.split(/\s/).select do |value|
+        value.length > 0
+      end
+    end
+  rescue
+    nil
+  end
+end
+
 # Batch 1, offset 0
 
 csv_list = Dir["./data/**/*.csv"]
 
 csv_list.each do |filename|
-  table = CSV.parse(filename, headers: true)
-  binding.pry
+  File.open(filename) do |f|
+    f.gets
+    csv = CSV.new(f, headers: true)
+    irradiance = read_sms_irradiance_log(filename.sub(".csv", ".sms"))
+    puts irradiance.inspect if irradiance
+  end
 end
+
