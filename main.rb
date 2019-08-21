5863620bee500ea4eb0680575891cfd437a2e6fa
Jon Stokes <jon.m.stokes@gmail.com>
2019-08-21 13:40:46 -0500

    Appears to be working

--- main.rb
+++ main.rb
@@ -4,7 +4,9 @@ require 'pry'
 # Common functions
 def read_sms_irradiance_log(filename)
   begin
-    File.read(filename).split("\n").map do |line|
+    irradiance_log = File.read(filename).split("\n")
+    irradiance_log.shift
+    irradiance_log.map do |line|
       line.split(/\s/).select do |value|
         value.length > 0
       end
@@ -19,11 +21,18 @@ end
 csv_list = Dir["./data/**/*.csv"]
 
 csv_list.each do |filename|
-  File.open(filename) do |f|
-    f.gets
-    csv = CSV.new(f, headers: true)
-    irradiance = read_sms_irradiance_log(filename.sub(".csv", ".sms"))
-    puts irradiance.inspect if irradiance
+  File.open(filename) do |multimeter_logs|
+    multimeter_logs.gets
+    table = CSV.new(multimeter_logs, headers: true).read
+    if irradiance = read_sms_irradiance_log(filename.sub(".csv", ".sms"))
+      CSV.open(filename.sub("data", "output"), "w") do |collated_logs|
+        collated_logs << table.headers  + ["Irradiance"]
+        table.each_with_index do |row, i|
+          next unless row["Irradiance"] = irradiance[i] && irradiance[i].last
+          collated_logs << row
+        end
+      end      
+    end
   end
 end
 
