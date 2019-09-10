017b590786af8357f3668e5c0fd150632f419197
Jon Stokes <jon.m.stokes@gmail.com>
2019-09-09 19:59:34 -0500

    Fix sample rate

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require './panel_stats.rb'
 
 Dir["./data/**/*.csv"].each do |filename|
-  opts = filename[/data\/8\//] ? { sample_rate: 10 } : {}
+  opts = filename[/data\/8\//] ? { sample_rate: 5 } : {}
   panel_stats = PanelStats.new(filename, opts)
   panel_stats.write_results!
 end
